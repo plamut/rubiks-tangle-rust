cc482e33539cf3511541acc276ce670e1a85ccf7
Peter Lamut <inbox@peterlamut.com>
2023-11-25 14:37:36 +0200

    Initial commit (base project layout)

---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+// TODO: Implement most of the code here and import in main.rs
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+// TODO: learn how to use rust formatter
+// TODO: integrate into VS code
+
+fn main() {
+    println!("Hello, world!");
+}
